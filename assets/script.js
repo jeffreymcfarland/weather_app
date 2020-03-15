86ceee06c49883a5ff13e075077cecbee54ed976
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-03-14 19:37:07 -0500

    set up files needed, tested links to each file, create urls for ajax calls

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+
+// var apiKey = "ba5f08a1af110cb53f0aab50ebe1553b";
+
+var futureDayURL = "api.openweathermap.org/data/2.5/forecast?q=" + city +"," + state + "&units=imperial&appid=ba5f08a1af110cb53f0aab50ebe1553b";
+
+var city  = "tallahassee";
+var state = "florida";
+
+var currentDayURL = "api.openweathermap.org/data/2.5/weather?q=" + city +"," + state + "&units=imperial&appid=ba5f08a1af110cb53f0aab50ebe1553b"
