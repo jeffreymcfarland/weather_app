443c3720afa6799ed662bab304c262457740b32c
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-03-16 19:43:31 -0500

    created variables for all data needed from ajax call and put some of those variables on the page

--- assets/script.js
+++ assets/script.js
@@ -6,8 +6,13 @@
 var city  = "";
 var state = "";
 
-var cityInput = $("#searchCity");
+var cityInput  = $("#searchCity");
 var stateInput = $("#searchState");
+
+var dateArray = [];
+var iconArray = [];
+var tempArray = [];
+var humArray  = [];
 
 function runQuery(){
 
@@ -22,13 +27,19 @@ $.ajax({
     url: futureDayURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response);
 
     // Grab date for each day
+    var date1 = moment().add(1, 'days').format("l");
+    var date2 = moment().add(2, 'days').format("l");
+    var date3 = moment().add(3, 'days').format("l");
+    var date4 = moment().add(4, 'days').format("l");
+    var date5 = moment().add(5, 'days').format("l");
 
+    dateArray.push(date1, date2, date3, date4, date5);
+    
+    console.log(dateArray);
 
     // Grab weather icon for each day
-
     var iconcode1 = response.list[4].weather[0].icon;
     var iconcode2 = response.list[12].weather[0].icon;
     var iconcode3 = response.list[20].weather[0].icon;
@@ -41,72 +52,96 @@ $.ajax({
     var iconurl4 = "http://openweathermap.org/img/w/" + iconcode4 + ".png";
     var iconurl5 = "http://openweathermap.org/img/w/" + iconcode5 + ".png";
 
-    // Grab temperature for each day
+    iconArray.push(iconurl1, iconurl2, iconurl3, iconurl4, iconurl5);
 
-    console.log((response.list[4].main.temp).toFixed(1) + " °F");
-    console.log((response.list[12].main.temp).toFixed(1) + " °F");
-    console.log((response.list[20].main.temp).toFixed(1) + " °F");
-    console.log((response.list[28].main.temp).toFixed(1) + " °F");
-    console.log((response.list[36].main.temp).toFixed(1) + " °F");
+    console.log(iconArray);
+
+    // Grab temperature for each day
+    var temp1 = (response.list[4].main.temp).toFixed(1) + " °F";
+    var temp2 = (response.list[12].main.temp).toFixed(1) + " °F";
+    var temp3 = (response.list[20].main.temp).toFixed(1) + " °F";
+    var temp4 = (response.list[28].main.temp).toFixed(1) + " °F";
+    var temp5 = (response.list[36].main.temp).toFixed(1) + " °F";
+
+    tempArray.push(temp1, temp2, temp3, temp4, temp5);
+
+    console.log(tempArray);
 
     // Grab humidity for each day
+    var hum1 = Math.round(response.list[4].main.humidity) + "%";
+    var hum2 = Math.round(response.list[12].main.humidity) + "%";
+    var hum3 = Math.round(response.list[20].main.humidity) + "%";
+    var hum4 = Math.round(response.list[28].main.humidity) + "%";
+    var hum5 = Math.round(response.list[36].main.humidity) + "%";
 
+    humArray.push(hum1, hum2, hum3, hum4, hum5);
 
+    console.log(humArray);
+    
   });
 
 $.ajax({
     url: currentDayURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response);
 
     // Grab city name
-
-    console.log(response.name);
-
+    var city = response.name;
 
     // Grab date
+    var date = moment().format("l");
 
-    console.log(moment().format("l"));
+    // Put City name on page
+    $(".current-day").addClass("border");
+    $(".cityName").text(city + " " + date).addClass("ml-5 mt-4");
 
+    // Grab weather icon
+    var icon = response.weather[0].icon;
+    var iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
 
     // Grab temperature
+    var temp = (response.main.temp).toFixed(1) + " °F";
 
-    console.log((response.main.temp).toFixed(1) + " °F");
+    // Put Temp on page
+    $(".cityTemp").text("Temperature: " + temp).addClass("ml-5 mt-4");
 
     // Grab humidity
+    var hum = Math.round(response.main.humidity) + "%";
 
-    console.log(Math.round(response.main.humidity) + "%");
+    // Put Humidity on page
+    $(".cityHum").text("Humidity: " + hum).addClass("ml-5 mt-4");
 
     // Grab wind speed
+    var wind = (response.wind.speed).toFixed(1) + "MPH";
 
-    console.log((response.wind.speed).toFixed(1) + "MPH");
+    // Put Wind Speed on page
+    $(".cityWind").text("Wind Speed: " + wind).addClass("ml-5 mt-4");
 
     // Grab latitude and longitude
-
-    console.log(response.coord.lon);
-    console.log(response.coord.lat);
     var lon = response.coord.lon;
     var lat = response.coord.lat;
 
     // URL for current UV Index
-  var UVIndexURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=ba5f08a1af110cb53f0aab50ebe1553b";
+    var UVIndexURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=ba5f08a1af110cb53f0aab50ebe1553b";
+
 
   $.ajax({
     url: UVIndexURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response);
 
     // Grab UV index
-    console.log(response.value);
+    var UV = response.value;
 
+    // Add class to UV
+    var span = $("<span>").text(UV).addClass("UV-bad");
+
+    // Put UV on page
+    $(".cityUV").text("UV Index: ").addClass("ml-5 mt-4").append(span);
 
   });
 
-
   });
-
 
 };
 
@@ -115,4 +150,6 @@ $("#search-btn").on("click", function(e){
     city  = cityInput.val().trim().toLowerCase();
     state = stateInput.val().trim().toLowerCase();
     runQuery();
+    cityInput.val("");
+    stateInput.val("");
   });
